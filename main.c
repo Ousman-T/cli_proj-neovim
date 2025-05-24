5fc7b3d9c0283d05798a78c46e6512ec3fe5faa0
Ousman Touray <otouray14@gmail.com>
2025-05-24 18:56:36 -0400

    need to tweak control flow

--- main.c
+++ main.c
@@ -1,13 +1,29 @@
 #include <math.h>
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>
 
-void main(void) {
-	srand(time(NULL));
-	int target = rand() % 100 + 1;
-	int input;
+int main(void) {
+  srand(time(NULL));
+  int target = rand() % 100 + 1;
+  int input;
+  int count = 0;
 
-	printf("I'm thinking of a number from 1-100\n");
-	scanf("%d", input);
+  printf("I'm thinking of a number from 1-100\n");
+  scanf("%d", &input);
+  while (input != target) {
+    if (input < target) {
+      printf("Guess too low!\n");
+      count++;
+      break;
+    } else if (input > target) {
+      printf("guess too high!\n");
+      count++;
+      break;
+    } else {
+      printf("You got it, nice! It only took you %d tries\n");
+      break;
+    }
+  }
+  return 0;
 }
